7908bf129147167d4c4d475bbb280b567e58ddbe
farzonshomurodzoda <somurodovfarzon@gmail.com>
2025-03-07 10:02:59 +0600

    Finish lab27 and its homeworks.

--- Lab18/Part5.py
+++ Lab18/Part5.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     names = ["Adilet", "Aziret", "Aidai"]
-    return render_template('index.html', names=names, title="Welcome")
+    return render_template('index_lab1.html', names=names, title="Welcome")
 
 if __name__ == '__main__':
     app.run(debug=True)
