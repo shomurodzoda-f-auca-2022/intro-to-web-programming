d88d2062b3803339b5784bc995d82723c4c2c507
farzonshomurodzoda <somurodovfarzon@gmail.com>
2025-04-29 21:03:30 +0600

    Add Images to the background, and add validators for login and registers

--- Final-Project/app/views.py
+++ Final-Project/app/views.py
@@ -44,7 +44,7 @@ def login():
 def logout():
     logout_user()
     flash('You have been logged out.', 'info')
-    return redirect(url_for('main.login'))
+    return redirect(url_for('main.home'))
 
 @main.route('/movies')
 @login_required
