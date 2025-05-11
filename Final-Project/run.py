2b1c9eacb5bc57973861a4d993759bd886d5075d
farzonshomurodzoda <somurodovfarzon@gmail.com>
2025-05-11 13:22:38 -0500

    Style the codes, and add some instances of db

--- Final-Project/run.py
+++ Final-Project/run.py
@@ -3,6 +3,6 @@ from app import create_app, seed_movies
 app = create_app()
 
 if __name__ == '__main__':
+    app.run(debug=True)
     with app.app_context():
         seed_movies()
-    app.run(debug=True)
