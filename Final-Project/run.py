fe4dff1b6b66e0bbaf3539815ebcf6382403146e
farzonshomurodzoda <somurodovfarzon@gmail.com>
2025-05-11 13:27:42 -0500

    Add more movies to the custom list.

--- Final-Project/run.py
+++ Final-Project/run.py
@@ -3,6 +3,7 @@ from app import create_app, seed_movies
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
     with app.app_context():
         seed_movies()
+
+    app.run(debug=True)
